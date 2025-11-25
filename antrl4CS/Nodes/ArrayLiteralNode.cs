d74c6aa65805678a2c2295555b04819c224415cd
selinefeliz <92600472+selinefeliz@users.noreply.github.com>
2025-11-25 11:58:08 -0500

    Add ArrayLiteralNode class with Elements property

---
+++ antrl4CS/Nodes/ArrayLiteralNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antrl4CS.Nodes
+{
+  public class ArrayLiteralNode : ExpressionNode
+  {
+      //arg_list
+      public List<ExpressionNode> Elements { get; } = [];
+  }
+}
